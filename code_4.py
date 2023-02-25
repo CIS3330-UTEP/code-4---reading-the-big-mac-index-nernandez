98f88d9e045385477aee965dd344d6350262283b
nernandez <70415076+nernandez@users.noreply.github.com>
2023-02-24 17:23:54 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -15,19 +15,33 @@ def get_big_mac_price_by_country(country_code):
     country_code = country_code.upper()
     df = pd.read_csv(big_mac_file)
     querry = f"(iso_a3 == '{country_code}')"
-    pby_df = df.query(querry)
-    min_idk = pby_df['dollar_price'].idxmin()
-    comb_QR = (pby_df.loc[min_idk]['dollar_price'])
-    return round(comb_QR,2)
+    pbc_df = df.query(querry)
+    min_idk = pbc_df['dollar_price'].idxmin()
+    xcomb_QR = (pbc_df.loc[min_idk]['dollar_price'])
+    return round(xcomb_QR,2)
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    df = pd.read_csv(big_mac_file)
+    querry = f"(date >='{year}-01-01' and date <= '{year}-12-31')"
+    cpby_df = df.query(querry)
+    min_idk = cpby_df['dollar_price'].idxmin()
+    ycomb_QR = (cpby_df.loc[min_idk]['dollar_price'])
+    return round(ycomb_QR,2)
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+       df = pd.read_csv(big_mac_file)
+       querry = f"(date >='{year}-01-01' and date <= '{year}-12-31')"
+       xpby_df = df.query(querry)
+       max_idk = xpby_df['dollar_price'].idxmax()
+       zcomb_QR = (xpby_df.loc[max_idk]['dollar_price'])
+       return round(zcomb_QR,2)
 
 if __name__ == "__main__":
-    Q1 = get_big_mac_price_by_year(2006,"mex")
+    Q1 = get_big_mac_price_by_year(2010,"arg")
     print(Q1)
     Q2 = get_big_mac_price_by_country("mex")
     print(Q2)
+    Q3 = get_the_cheapest_big_mac_price_by_year("2008")
+    print(Q3)
+    Q4 = get_the_most_expensive_big_mac_price_by_year("2014")
+    print(Q4)
